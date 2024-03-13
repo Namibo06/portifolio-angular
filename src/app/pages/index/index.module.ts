82e317dccd5667f7459f26b7ae079af79b3b4f01
Namibo06 <namibo654@gmail.com>
2024-03-13 11:57:26 -0300

    build:index

--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
 import { HeaderModule } from '../../components/header/header.module';
 import { FooterModule } from '../../components/footer/footer.module';
+import {register} from 'swiper/element/bundle';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+register()
 
 
 @NgModule({
@@ -12,7 +15,9 @@ import { FooterModule } from '../../components/footer/footer.module';
   imports: [
     CommonModule,
     HeaderModule,
-    FooterModule
-  ]
+    FooterModule,
+    FontAwesomeModule
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class IndexModule { }
--- src/app/pages/index/index/index.component.ts
+++ src/app/pages/index/index/index.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faAngular, faCss3, faHtml5, faJava, faJs, faLaravel, faPhp, faSass } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-index',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './index.component.sass'
 })
 export class IndexComponent {
-
+  html:IconDefinition=faHtml5;
+  css:IconDefinition=faCss3;
+  sass:IconDefinition=faSass;
+  js:IconDefinition=faJs;
+  angular:IconDefinition=faAngular;
+  php:IconDefinition=faPhp;
+  laravel:IconDefinition=faLaravel;
+  java:IconDefinition=faJava;
+  database:IconDefinition=faDatabase;
 }
