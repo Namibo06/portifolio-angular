fcf26e530d1229fa6448c349099d30d0034552ee
Namibo06 <namibo654@gmail.com>
2024-03-12 23:36:54 -0300

    style:footer

--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
+  exports:[FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
+    RouterModule
   ]
 })
 export class FooterModule { }
--- src/app/components/footer/footer/footer.component.ts
+++ src/app/components/footer/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {faFacebook,faLinkedin, faWhatsapp} from  '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.sass'
 })
 export class FooterComponent {
+  whatsapp:IconDefinition=faWhatsapp;
+  linkedIn:IconDefinition=faLinkedin;
+  gmail:IconDefinition=faEnvelope;
 
+  constructor(){}
 }
