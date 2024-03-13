f860eb28d7a6dfa5a42b7384d09d12e086c7d52c
Namibo06 <namibo654@gmail.com>
2024-03-13 15:51:01 -0300

    style:index

--- src/app/components/header/header/header.component.ts
+++ src/app/components/header/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 import {faBars} from "@fortawesome/free-solid-svg-icons";
@@ -12,6 +12,10 @@ export class HeaderComponent {
   open_menu:IconDefinition=faBars;
   menu_mobile:boolean=false;
 
+  @Output() downAbout = new EventEmitter<any>();
+  @Output() downStacks = new EventEmitter<any>();
+  @Output() downProjects = new EventEmitter<any>();
+
   constructor(){}
 
   open_menu_mobile(){
@@ -20,5 +24,17 @@ export class HeaderComponent {
 
   close_menu_mobile(){
     this.menu_mobile=false;
+  }
+
+  about(){
+    this.downAbout.emit(null);
+  }
+
+  stacks(){
+    this.downStacks.emit(null);
+  }
+
+  projects(){
+    this.downProjects.emit(null);
   }
 }
--- src/app/pages/index/index/index.component.ts
+++ src/app/pages/index/index/index.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './index.component.sass'
 })
 export class IndexComponent {
+  constructor(){}
+
+  about(){
+    const about=document.getElementById('about');
+    about?.scrollIntoView({behavior:'smooth'});
+  }
+
+  stacks(){
+    const stacks=document.getElementById('stacks');
+    stacks?.scrollIntoView({behavior:'smooth'});
+  }
+
+  projects(){
+    const projects=document.getElementById('projects');
+    projects?.scrollIntoView({behavior:'smooth'});
+  }
 }
